5e9072ad25120101d7673ab04c7e147772bcf24d
Felucco <matteo.mannis@libero.it>
2022-10-09 18:19:14 +0200

    BSHR: implemented Mk2 correction

--- lib/Bouncing_SHR.v
+++ lib/Bouncing_SHR.v
@@ -8,6 +8,7 @@ module Bouncing_SHR #(
 );
     wire shr_en, shr_sout;
     reg l_nr;
+    wire l_nr_in;
     wire [N_BIT-1:0] shr_pin;
 
     assign shr_en=en|sin;
@@ -16,13 +17,10 @@ module Bouncing_SHR #(
     SHR #(.N_BIT(N_BIT)) core_SHR (.clk(clk), .rst(rst), .en(shr_en), .l_nr(l_nr),
                                     .pl(1'b0), .sin(sin), .pin(shr_pin), .sout(shr_out), .pout(pout));
     
+    assign l_nr_in = l_nr ? ~pout[N_BIT-1] : pout[0];
+
     always @(posedge clk or posedge rst) begin
         if (rst) l_nr<=0;
-        else begin
-            case (l_nr)
-                1'b1: l_nr <= ~pout[N_BIT-2]; 
-                default: l_nr <= pout[1];
-            endcase
-        end
+        else l_nr<=l_nr_in;
     end
 endmodule
